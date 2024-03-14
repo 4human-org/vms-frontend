886e8be4030d21278892824f2773d814b1da8005
Aman Khan <khan@Amans-MacBook-Pro-3.local>
2024-03-14 14:46:27 -0700

    Login and footer initial setup

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#fcd137] text-right text-black font-bold p-4 pr-14">
+      <a href = "">Our Mission</a>
+    </footer>
+  );
+};
+
+export default Footer;
