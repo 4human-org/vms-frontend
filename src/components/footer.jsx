ba6b78c4bfa7a4e028f4fca1e440a3a0e25df53f
DylanT80 <DylanTTran80@gmail.com>
2024-03-14 16:42:03 -0700

    Project created with Vite instead of create-react-app

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#fcd137] text-right text-black font-bold p-4 pr-14">
+      <a href = "">Our Mission</a>
+    </footer>
+  );
+};
+
+export default Footer;
