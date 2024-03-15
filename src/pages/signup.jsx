ea07513a46b7b712021f15990a44344a56e27c20
Sutantu Balamurugan <sutan04@gmail.com>
2024-03-14 18:20:13 -0700

    Slight changes Added overflow hidden to body css and started signup page

---
+++ src/pages/signup.jsx
@@ -0,0 +1,12 @@
+import Footer from '../components/footer';
+
+const SignUpPage = () => {
+    return (
+        <div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default SignUpPage;
